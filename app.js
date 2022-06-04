74a0e65d80bf7a05544f818c17b1423f8dec9f9f
Uğur Mercan <71211251+KeazoN@users.noreply.github.com>
2022-06-04 14:21:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,30 +2,63 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const util = require('util');
 const app = express();
+const router = express.Router();
 const spotifyApi = new SpotifyWebApi({
-  clientId: 'clientId',
-  clientSecret: 'clientSecret'
-  //https://developer.spotify.com You can get informations from
+  clientId: 'a8bd3984f8ae4049a5395f77a0da535c',
+  clientSecret: '47b8428c8bd044eea28c7bf1a47be8a9'
 });
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+	res.render('index');
+});
+
+app.get('/user', (req, res) => {
+	global.userData;
+	global.playlistData;
+	global.totalData = [];
+	let token = "q34hz9w6r3i1jhxrn5kxk0dyp";
+
 	spotifyApi.clientCredentialsGrant()
-		.then((data) => {
-		    spotifyApi.setAccessToken(data.body['access_token']);
-		    return spotifyApi.getPlaylist('id');
-		    //https://open.spotify.com/playlist/id You can get id from
+		.then((playlist) => {
+		    spotifyApi.setAccessToken(playlist.body['access_token']);
+		    return spotifyApi.getUserPlaylists(token);
 		 })
-		.then((data) => {
-			res.render('index', {data: data.body});
-			//console.log(util.inspect(data.body.tracks.items[1].track, false, null, true));
-			//console.log(util.inspect(data.body, false, null, true));
+		.then((playlist) => {
+			global.playlistData = playlist.body;
 		  })
 		  .catch((err) => {
 		    console.log(err);
 		  });
+
+	spotifyApi.clientCredentialsGrant()
+		.then((user) => {
+		    spotifyApi.setAccessToken(user.body['access_token']);
+		    return spotifyApi.getUser(token);
+		 })
+		.then((user) => {
+			global.userData = user.body;
+		  })
+		  .catch((err) => {
+		    console.log(err);
+		  });
+	res.render('user', {playlist: playlistData, user: userData});
 });
+
+app.get('/playlist/:id', (req, res, next) => {
+  spotifyApi.clientCredentialsGrant()
+		.then((data) => {
+		    spotifyApi.setAccessToken(data.body['access_token']);
+		    return spotifyApi.getPlaylist(req.params.id);
+		 })
+		.then((data) => {
+			res.render('playlist', {data: data.body});
+		  })
+		  .catch((err) => {
+		    console.log(err);
+		  });
+})
 
 app.listen(5000);
